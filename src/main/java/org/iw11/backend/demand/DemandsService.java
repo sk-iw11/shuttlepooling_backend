2e0f967218831df254463b0fe4794f469f815ecc
TimeEscaper <rentgeny05@gmail.com>
2019-09-17 14:16:57 +0300

    Demands service implemented

---
+++ src/main/java/org/iw11/backend/demand/DemandsService.java
@@ -0,0 +1,34 @@
+package org.iw11.backend.demand;
+
+import org.iw11.backend.model.BusDemand;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * TODO: Check concurrency model
+ */
+@Service
+public class DemandsService {
+
+    private Map<BusDemand, AtomicInteger> demandsMap = new ConcurrentHashMap<>();
+
+    public void putDemand(BusDemand demand) {
+        demandsMap.putIfAbsent(demand, new AtomicInteger(1)).incrementAndGet();
+    }
+
+    public void satisfyDemand(BusDemand demand) {
+        demandsMap.remove(demand);
+    }
+
+    public Map<BusDemand, Integer> getDemands() {
+        var demands = new HashMap<BusDemand, Integer>();
+        demandsMap.entrySet().forEach((entry -> demands.put(entry.getKey(), entry.getValue().get())));
+        return demands;
+    }
+}
