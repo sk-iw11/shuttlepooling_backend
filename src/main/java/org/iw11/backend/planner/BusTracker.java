1fa1a13a98cee66b646e0e1ada9443a60da0de72
TimeEscaper <rentgeny05@gmail.com>
2019-09-26 06:48:19 +0300

    Route optimization by location implemented

--- src/main/java/org/iw11/backend/planner/BusTracker.java
+++ src/main/java/org/iw11/backend/planner/BusTracker.java
@@ -5,6 +5,7 @@ import org.iw11.backend.config.BusesConfigService;
 import org.iw11.backend.model.BusDemand;
 import org.iw11.backend.model.BusRoute;
 import org.iw11.backend.model.GeoCoordinates;
+import org.iw11.backend.util.CoordinatesUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -62,10 +63,24 @@ public class BusTracker {
     public void assignRoutes(List<BusRoute> routes) {
         if (routes.isEmpty())
             return;
-        var routeIterator = routes.iterator();
-        buses.entrySet().stream()
-                .filter(bus -> bus.getValue().isAvailable())
-                .limit(routes.size())
-                .forEach(bus -> bus.getValue().setCurrentRoute(routeIterator.next()));
+
+        routes.forEach(route -> {
+            var cl = buses.entrySet().stream()
+                    .filter(bus -> bus.getValue().isAvailable() && bus.getValue().getCurrentLocation().isPresent())
+                    .min((busA, busB) ->
+                        compareDistances(busA.getValue().getCurrentLocation(),
+                                busB.getValue().getCurrentLocation(), route.getStations().get(0).getLocation()));
+            if (cl.isPresent()) {
+                cl.get().getValue().setCurrentRoute(route);
+            }
+        });
+    }
+
+    private int compareDistances(Optional<GeoCoordinates> pointA, Optional<GeoCoordinates> pointB, GeoCoordinates pointC) {
+        if (pointA.isEmpty())
+            return 1;
+        if (pointB.isEmpty())
+            return -1;
+        return CoordinatesUtil.getDistance(pointA.get(), pointC) > CoordinatesUtil.getDistance(pointB.get(), pointC) ? 1 : -1;
     }
 }
